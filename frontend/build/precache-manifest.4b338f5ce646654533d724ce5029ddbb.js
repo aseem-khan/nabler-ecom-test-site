83e5102fef155da5892724b29993c7e80a3b31e4
Aseem Khan Work <aseem.k@nabler.com>
2023-03-22 13:41:42 +0530

    added build folder

---
+++ frontend/build/precache-manifest.4b338f5ce646654533d724ce5029ddbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8955eeb77a6f513717b4e62f5caea341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01d138169849dc0febc6",
+    "url": "/static/css/main.010fcd9a.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01d138169849dc0febc6",
+    "url": "/static/js/main.eb76a649.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
